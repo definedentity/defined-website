137da351970509420367d542455087c444c36d81
Nguyen Minh Tho <definedentity@proton.me>
2024-10-28 05:14:00 +0700

    Clean files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { AboutMe } from "@/components/about-me"
 import { Footer } from "@/components/footer"
 import { Header } from "@/components/header"
-import { ProjectContainer } from "@/components/project-container"
+import { ProjectContainer } from "@/components/project"
 
 export default function Home() {
 	return (
